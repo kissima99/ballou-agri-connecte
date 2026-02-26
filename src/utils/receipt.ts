aa1328fe5d0cfc813ea4d7d70141fecb113a9fd7
[dyad] <ramatayaha003@gmail.com>
2026-02-26 12:54:40 +0000

    Updated src/utils/receipt.ts

--- src/utils/receipt.ts
+++ src/utils/receipt.ts
@@ -32,7 +32,7 @@ export const generateReceipt = (data: ReceiptData) => {
 
     doc.setFontSize(12);
     doc.setTextColor(secondaryColor);
-    doc.text('BALLOU AGRI CONNECT', 105, 20, { align: 'center' });
+    doc.text('BALLOU AGRI CONNECT', 105, 20, { align: 'center' } as any);
 
     // Ligne
     doc.setLineWidth(0.5);
