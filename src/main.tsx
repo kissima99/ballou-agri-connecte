b420c7a215f56bd018dd6f761c9d163b85828f2a
[dyad] <ramatayaha003@gmail.com>
2026-04-09 00:49:12 +0000

    [dyad] Installation et activation de Vercel Analytics - wrote 3 file(s), added @vercel/analytics package(s)

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,10 @@ import App from './App';
 import './globals.css';
 import { CartProvider } from './context/CartContext';
 import { Toaster } from "@/components/ui/sonner";
+import { inject } from '@vercel/analytics';
+
+// Initialisation de Vercel Analytics
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
