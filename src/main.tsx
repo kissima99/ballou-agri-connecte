9666d2cdc8b800988b6f1905864d7c0a5b9c6d18
[dyad] <ramatayaha003@gmail.com>
2026-04-13 23:53:59 +0000

    [dyad] Correction de l'erreur de rendu React DOM dans le fichier main.tsx - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,22 @@
-"use client";
-
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './globals.css';
 import { CartProvider } from './context/CartContext';
 import { Toaster } from "@/components/ui/sonner";
 import { Analytics } from '@vercel/analytics/react';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <CartProvider>
-      <App />
-      <Analytics />
-      <Toaster position="top-center" />
-    </CartProvider>
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <CartProvider>
+        <App />
+        <Analytics />
+        <Toaster position="top-center" />
+      </CartProvider>
+    </React.StrictMode>
+  );
+}
